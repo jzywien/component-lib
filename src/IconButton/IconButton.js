6a29c159e71a24423b3fd3b6458747c179217d9f
Harry <harrythedev@gmail.com>
2018-04-30 17:11:53 -0400

    added styled component: breaking props

--- src/IconButton/IconButton.js
+++ src/IconButton/IconButton.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-md';
+import styled from 'styled-components';
 
-import './IconButton.scss';
+class ButtonComponent extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    icon: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    showIcon: PropTypes.bool
+  }
+  
+  static defaultProps = {
+    icon: 'star',
+    showIcon: true
+  }
 
-
-function IconButton(props) {
-  const { classes, icon, label, onClick, showIcon } = props;
-  return (
-    <Button className='IconButton' onClick={onClick} raised primary iconClassName={'material-icons'} iconChildren={showIcon && icon}>
-      {label}
-    </Button>
-  );
+  render() {
+    const { className, classes, icon, label, onClick, showIcon } = this.props;
+    return (
+      <Button className={`IconButton ${className}`} onClick={onClick} raised primary iconClassName={'material-icons'} iconChildren={showIcon && icon}>
+        {label}
+      </Button>
+    );
+  }
 }
-
-IconButton.propTypes = {
-  classes: PropTypes.object.isRequired,
-  icon: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-  showIcon: PropTypes.bool
-};
-
-IconButton.defaultProps = {
-  icon: 'star',
-  showIcon: true
-}
-
+const IconButton = styled(ButtonComponent)`
+  color: red;
+  .md-icon-text {
+    font-weight: bold;
+    &:last-child {
+      padding-left: 4px;
+    }
+    text-align: center;
+  }
+`;
 export default IconButton;
--- src/SimpleExpansionPanel/SimpleExpansionPanel.js
+++ src/SimpleExpansionPanel/SimpleExpansionPanel.js
@@ -1,46 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ExpansionPanel from 'react-md/lib/ExpansionPanels/ExpansionPanel';
 import './SimpleExpansionPanel.scss';
 
-function SimpleExpansionPanel(props) {
-  const { classes, children, icon, title, count, showCount, headerClassName, contentClassName, lightTheme } = props;
-
-  const classNames = `SimpleExpansionPanel ${lightTheme ? 'SimpleExpansionPanel-light' : ''}`
-  return (
-    <ExpansionPanel className={classNames} headerClassName={headerClassName} contentClassName={'expansion-content ' + contentClassName} label={
-        <div style={{display: 'flex', alignItems: 'center'}
-        }>
-          <i class="md-icon material-icons" style={{marginRight: '5px'}}>{icon}</i>
-          {title}
-          {showCount &&
-            <span className="count">{count}</span>
-          }
-        </div>
-      }  defaultExpanded footer={null}>
-      {children}
-    </ExpansionPanel>
-  );
-}
-
-SimpleExpansionPanel.propTypes = {
-  children: PropTypes.node.isRequired,
-  classes: PropTypes.object.isRequired,
-  className: PropTypes.string,
-  headerClassName: PropTypes.string,
-  contentClassName: PropTypes.string,
-  icon: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  count: PropTypes.number.isRequired,
-  showCount: PropTypes.bool,
-  lightTheme: PropTypes.bool
-};
-
-SimpleExpansionPanel.defaultProps = {
-  icon: 'star',
-  count: 0,
-  showCount: true,
-  lightTheme: false
+class SimpleExpansionPanel extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    classes: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    headerClassName: PropTypes.string,
+    contentClassName: PropTypes.string,
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired,
+    showCount: PropTypes.bool,
+    lightTheme: PropTypes.bool  
+  };
+  static defaultProps = {
+    icon: 'star',
+    count: 0,
+    showCount: true,
+    lightTheme: false
+  }
+  
+  render() {
+    const { className, classes, children, icon, title, count, showCount, headerClassName, contentClassName, lightTheme } = this.props;
+  
+    const classNames = `SimpleExpansionPanel ${lightTheme ? 'SimpleExpansionPanel-light' : ''} ${className}`
+    return (
+      <ExpansionPanel className={classNames} headerClassName={headerClassName} contentClassName={'expansion-content ' + contentClassName} label={
+          <div style={{display: 'flex', alignItems: 'center'}
+          }>
+            <i class="md-icon material-icons" style={{marginRight: '5px'}}>{icon}</i>
+            {title}
+            {showCount &&
+              <span className="count">{count}</span>
+            }
+          </div>
+        }  defaultExpanded footer={null}>
+        {children}
+      </ExpansionPanel>
+    );
+  }
 }
 
 export default SimpleExpansionPanel;
